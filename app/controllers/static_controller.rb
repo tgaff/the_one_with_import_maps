9c7f62981a02ff99a302ab3a1b5405c35cdc4b6a
Travis Gaff <travis@labzero.com>
2023-03-24 11:55:00 +0900

    slow endpoint - but doesn't prove anything

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,9 @@
 class StaticController < ApplicationController
+
   def home
+  end
+
+  def slow_streaming
+    @times = 10_000
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'static/home'
+  get 'slow', to: 'static#slow_streaming'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
