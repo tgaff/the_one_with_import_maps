230ef980ba45b1716a45647f3fddb320719ad03b
Travis Gaff <travis@labzero.com>
2023-03-30 12:38:25 +0900

    importmap pin react react-dom htm/react

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,8 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "three", to: "https://ga.jspm.io/npm:three@0.150.1/build/three.module.js"
+pin "htm/react", to: "https://ga.jspm.io/npm:htm@3.1.1/react/index.module.js"
+pin "react", to: "https://ga.jspm.io/npm:react@18.2.0/index.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@18.2.0/index.js"
+pin "htm", to: "https://ga.jspm.io/npm:htm@3.1.1/dist/htm.module.js"
+pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.23.0/index.js"
